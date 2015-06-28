ae2d61ffb3843b24430a7c3f2b889c7c96a99cb8
DarthMax <max@kopfueber.org>
2015-06-28 15:07:29 +0200

    improve speech recognition

--- public/javascript/speech_recognition.js
+++ public/javascript/speech_recognition.js
@@ -1,55 +1,104 @@
-var navigator = window.navigator;
-navigator.getUserMedia = (
-  navigator.getUserMedia ||
+function OnlineSpeechRecognition() {
+  this.recorder = null;
+  this.recording = false;
+  this.stream = null;
+
+  navigator.getUserMedia = (
+    navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia
-);
+  );
 
-var recorder
+  this.startRecognition = function() {
+    if(!this.recording) {
+      navigator.getUserMedia({audio: true}, function(stream){
+        this.stream=stream
+        var context = new AudioContext();
+        var mediaStreamSource = context.createMediaStreamSource(stream);
+        this.recorder = new Recorder(mediaStreamSource);
+        this.recording = true;
+        console.log(this);
+        this.recorder.record();
+      }.bind(this),function(error){
+        console.log(err)
+      });
+    }
+  }.bind(this);
 
-function startRecording() {
-   navigator.getUserMedia({audio: true}, micAccessGranted, micAccessDenied);
+  this.stopReccognition = function() {
+    console.log(this);
+    if(this.recording){
+      this.recording=false;
+      this.recorder.stop();
+      this.stream.stop();
+      this.recorder.exportWAV(function (sound_blob) {
+        var fd = new FormData();
+        fd.append('fname', 'speech.wav');
+        fd.append('data', sound_blob);
+        $.ajax({
+            type: 'POST',
+            url: '/speech_to_text',
+            data: fd,
+            processData: false,
+            contentType: false,
+            success: function(data) {
+              $('#search-input').val(data.hypothesis);
+            }
+        })
+      });
+    }
+  }.bind(this);
 }
 
-function stopRecording() {
-   recorder.stop();
-   recorder.exportWAV(function (blob) {
-       upload(blob);
-   });
-}
+function OfflineSpeechRecognition() {
+    this.transcript = ""
+    this.recognition = new webkitSpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    this.recognition.lang = "en-US";
 
-function micAccessDenied(error) {
-    console.log('Rejected!', error);
-};
+    this.recognition.onstart = function(){
+      console.log("Startet")
+    }
 
-function micAccessGranted(stream) {
-    var context = new AudioContext();
-    var mediaStreamSource = context.createMediaStreamSource(stream);
-    recorder = new Recorder(mediaStreamSource);
-    recorder.record();
-}
+    this.recognition.onerror = function(e) {
+      //console.log(e)
+    }
 
-function upload(soundBlob) {
-  var fd = new FormData();
-  fd.append('fname', 'speech.wav');
-  fd.append('data', soundBlob);
-  $.ajax({
-      type: 'POST',
-      url: '/speech_to_text',
-      data: fd,
-      processData: false,
-      contentType: false,
-      success: function(data) {
-        $('#search-input').val(data.hypothesis);
+    this.recognition.onspeechend = function(e) {
+      console.log("end");
+    }
+
+    this.recognition.onresult = function(event) {
+      interim_transcript = ""
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          this.transcript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
       }
-  })
+      $('#search-input').val(this.transcript + interim_transcript);
+    }.bind(this)
+
+    this.startRecognition = function(){
+      this.transcript = "";
+      this.recognition.start();
+    }
+
+    this.stopReccognition = function(){
+      this.recognition.stop();
+    }
 }
+
+window.SpeechRecognition = (typeof(webkitSpeechRecognition) != 'undefined') ? new OfflineSpeechRecognition : new OnlineSpeechRecognition
+
 
 $(document).on('click','#start-recording',function(){
-  startRecording();
+  window.SpeechRecognition.startRecognition()
 })
 
 $(document).on('click','#stop-recording',function(){
-  stopRecording();
+  window.SpeechRecognition.stopReccognition()
 })
